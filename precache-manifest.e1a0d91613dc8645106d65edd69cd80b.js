8c0d33267e548a18d68a36b1b78d4a50bf7e889b
Alish Giri <alishgiri.92@gmail.com>
2018-11-26 19:34:35 +0545

    Updates

---
+++ precache-manifest.e1a0d91613dc8645106d65edd69cd80b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4700e42360e08ab830dd",
+    "url": "/list-view-react/static/js/runtime~main.4700e423.js"
+  },
+  {
+    "revision": "781845e2285715f8681c",
+    "url": "/list-view-react/static/js/main.781845e2.chunk.js"
+  },
+  {
+    "revision": "4e8d2d64b6a92356aeb8",
+    "url": "/list-view-react/static/js/1.4e8d2d64.chunk.js"
+  },
+  {
+    "revision": "781845e2285715f8681c",
+    "url": "/list-view-react/static/css/main.c225981a.chunk.css"
+  },
+  {
+    "revision": "0b1437c39ce349042738d8cfab9b0c9d",
+    "url": "/list-view-react/index.html"
+  }
+];
